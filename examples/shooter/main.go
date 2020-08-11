4c82658104aaba3c52022868912ca5f147f3f632
papayajuice <mike@superofficial.net>
2020-08-10 23:01:33 -0400

    Add window funcs and enemies

--- examples/shooter/main.go
+++ examples/shooter/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/PapayaJuice/goose/internal/texture"
 
@@ -17,6 +19,10 @@ type Game struct {
 
 	bulletTexture texture.Texture
 	bullets       []*Bullet
+
+	enemyTexture texture.Texture
+	enemyTicker  *time.Ticker
+	enemies      []*Enemy
 
 	player *Player
 }
@@ -33,7 +39,16 @@ func (g *Game) Init() error {
 	g.player = &Player{
 		tex:   pt,
 		speed: 3,
+		x:     (goose.GetWindowX() / 2) - (pt.W() / 2),
+		y:     goose.GetWindowY() - pt.H() - 5,
 	}
+
+	et, err := goose.NewTexture("assets/enemy.png")
+	if err != nil {
+		return fmt.Errorf("error loading enemy texture: %v", err)
+	}
+	g.enemyTexture = et
+	g.enemyTicker = time.NewTicker(1 * time.Second)
 
 	bt, err := goose.NewTexture("assets/bullet.png")
 	if err != nil {
@@ -46,46 +61,84 @@ func (g *Game) Init() error {
 
 // Close ...
 func (g *Game) Close() error {
+	g.enemyTicker.Stop()
+
 	err := g.player.tex.Close()
 	if err != nil {
 		return fmt.Errorf("error closing player texture: %v", err)
+	}
+
+	err = g.enemyTexture.Close()
+	if err != nil {
+		return fmt.Errorf("error closing enemy texture: %v", err)
 	}
 
 	err = g.bulletTexture.Close()
 	if err != nil {
 		return fmt.Errorf("error closing bullet texture: %v", err)
 	}
+
 	return nil
 }
 
 // Update ...
 func (g *Game) Update() error {
+	// Input
 	if g.keyboard.IsKeyDown(input.KeyArrowLeft) {
 		g.player.x -= g.player.speed
+		if g.player.x < 0 {
+			g.player.x = 0
+		}
 	}
 	if g.keyboard.IsKeyDown(input.KeyArrowRight) {
 		g.player.x += g.player.speed
-	}
-	if g.keyboard.IsKeyDown(input.KeyArrowUp) {
-		g.player.y -= g.player.speed
-	}
-	if g.keyboard.IsKeyDown(input.KeyArrowDown) {
-		g.player.y += g.player.speed
+		if g.player.x > goose.GetWindowX()-g.player.tex.W() {
+			g.player.x = goose.GetWindowX() - g.player.tex.W()
+		}
 	}
 
 	if g.keyboard.IsKeyPress(input.KeySpace) {
 		b := &Bullet{
 			tex:   g.bulletTexture,
 			speed: 5,
-			x:     g.player.x,
-			y:     g.player.y,
+			x:     g.player.x + (g.bulletTexture.W() / 2),
+			y:     g.player.y - g.bulletTexture.H(),
 		}
 		g.bullets = append(g.bullets, b)
 	}
 
+	// Enemies
+	select {
+	case <-g.enemyTicker.C:
+		e := &Enemy{
+			tex:   g.enemyTexture,
+			speed: 2,
+			x:     rand.Int31n(goose.GetWindowX()),
+			y:     0,
+		}
+		g.enemies = append(g.enemies, e)
+	default:
+	}
+
+	remEnemies := []int{}
+	for i, e := range g.enemies {
+		if e.destroyed || e.y > goose.GetWindowY() {
+			remEnemies = append(remEnemies, i)
+			continue
+		}
+
+		e.y += e.speed
+	}
+
+	for _, ei := range remEnemies {
+		g.enemies[ei] = g.enemies[len(g.enemies)-1]
+		g.enemies = g.enemies[:len(g.enemies)-1]
+	}
+
+	// Bullets
 	remBullets := []int{}
 	for i, b := range g.bullets {
-		if b.destroyed {
+		if b.destroyed || b.y < 0 {
 			remBullets = append(remBullets, i)
 			continue
 		}
@@ -98,6 +151,27 @@ func (g *Game) Update() error {
 		g.bullets = g.bullets[:len(g.bullets)-1]
 	}
 
+	// This is terrible collision detection but whatever
+	for _, b := range g.bullets {
+		for _, e := range g.enemies {
+			if b.x > e.x+e.tex.W() {
+				continue
+			}
+			if b.x+b.tex.W() < e.x {
+				continue
+			}
+			if b.y > e.y+e.tex.H() {
+				continue
+			}
+			if b.y+b.tex.H() < e.y {
+				continue
+			}
+			// Otherwise, collision
+			b.destroyed = true
+			e.destroyed = true
+		}
+	}
+
 	return nil
 }
 
@@ -106,6 +180,13 @@ func (g *Game) Draw() error {
 	err := g.player.tex.Draw(g.player.x, g.player.y, 1.0, 1.0)
 	if err != nil {
 		return fmt.Errorf("error drawing player texture: %v", err)
+	}
+
+	for _, e := range g.enemies {
+		err = e.tex.Draw(e.x, e.y, 1.0, 1.0)
+		if err != nil {
+			return fmt.Errorf("error drawing enemy texture: %v", err)
+		}
 	}
 
 	for _, b := range g.bullets {
